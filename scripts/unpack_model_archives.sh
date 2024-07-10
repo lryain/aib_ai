447124bade37642b2c941b055f2249c128839c0a
lryain <liuhu_0618@sina.com>
2024-07-10 21:01:53 +0800

    update unpack script

--- scripts/unpack_model_archives.sh
+++ scripts/unpack_model_archives.sh
@@ -2,7 +2,8 @@
 # Downloads model compressed archives, extracts and moves to card locations.
 
 # Courtesy of https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(dirname $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ))
 
 # Licences and readme text files are saved here.
 MODEL_FOLDER="${SCRIPT_DIR}/models"
